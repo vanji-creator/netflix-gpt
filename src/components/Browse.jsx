2363039b4626c71e42da269a1442bbb962b379a4
vanji <vikashvanchi@gmail.com>
2024-05-07 17:34:23 +0530

    created a custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,22 +1,9 @@
-import React, { useEffect } from "react";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import HeaderBrowse from "./HeaderBrowse";
 import Navigator from "./Navigator";
-import { API_OPTIONS } from "../utils/constants";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json);
-  };
-
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
+  useNowPlayingMovies();
   return (
     <div>
       <Navigator />
