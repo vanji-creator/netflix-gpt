01ffa7d2e97be728192bfe6c68999f27f2cb8e6e
vanji <vikashvanchi@gmail.com>
2024-06-01 12:24:11 +0530

    web analytics update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Provider } from "react-redux";
 import Body from "./components/Body";
 import appStore from "./utils/appStore";
 import Contact from "./components/Contact";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <Provider store={appStore}>
       <Body />
+      <Analytics />
     </Provider>
   );
 }
